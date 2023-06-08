aa6d76c5017335617a06852124f4df1e6a5e6da3
Sheshmani Patel <sumitkumar47581@gmail.com>
2023-06-09 03:22:17 +0530

    Using product array and calling ir by map function

--- src/Cart.js
+++ src/Cart.js
@@ -1,17 +1,46 @@
 import React from "react";
-import CartItem  from "./Cartitem";
+import CartItem from "./Cartitem";
 
 class Cart extends React.Component {
- render(){
-    return(
-        <div className="cart">
-            <CartItem/>
-            <CartItem/>
-            <CartItem/>
-        </div>
-    )
- }
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 19999,
+          title: "Mobile Phone",
+          qty: 1,
+          img: " ",
+          id: 1,
+        },
+        {
+          price: 99999,
+          title: "Laptop",
+          qty: 1,
+          img: " ",
+          id: 2,
+        },
+        {
+          price: 9999,
+          title: "Watch",
+          qty: 10,
+          img: " ",
+          id: 3,
+        },
+      ],
+    };
+    // this.testing();
+  }
+  render() {
+    const { products } = this.state;
+    return (
+      <div className="cart">
+        {products.map((product) => {
+          return <CartItem product={product} key={product.id} />;
+        })}
+      </div>
+    );
+  }
 }
 
- 
 export default Cart;
